9983f76a206fdea8c6b5ca59e69db8782fba430c
Alexandre Wahl <alexandre.wahl12@gmail.com>
2026-02-18 01:19:05 -0300

    feat: botao sortear funcionando, funcao shuffle e validacao de entrada

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 //Funcao para adicionar nome digitado na lista de amigos e mostrar na tela os nomes da lista
 function adicionar(){
     let nomePessoa = document.getElementById('nome-amigo').value;
+
+    if (nomePessoa === ''){
+        alert('Você precisa informar um nome!!!');
+        return;
+    }
+    
     listaAmigos.push(nomePessoa);
 
     let listaNomes = document.getElementById('lista-amigos');
@@ -11,11 +17,30 @@ function adicionar(){
 }
 
 //Funcao para sortear amigos aleatóriamente e exibir amigos sorteados
-
+function sortear(){
+    if (listaAmigos.length < 2){
+        alert('Precisa de pelo menos 2 pessoas para realizar o sorteio!!!');
+        return;
+    }
+    let exibirSorteio = document.getElementById('lista-sorteio');
+    exibirSorteio.innerHTML = '';
+    let listaEmbaralhada = shuffle(listaAmigos);
+    for(let i = 0; i< listaAmigos.length; i++){
+        exibirSorteio.innerHTML += i==(listaAmigos.length-1) ? `${listaEmbaralhada[i]} -> ${listaEmbaralhada[0]}<br>`:`${listaEmbaralhada[i]} -> ${listaEmbaralhada[i+1]}<br>`;
+    }
+}
 
 //Funcao para reiniciar a lista de amigos secretos
 
 
-//Funcao para sortear nomes aleatoriamente
+
+//Algoritimo de Fisher-Yates para embaralhar um array (faz um swap entre a posicao i do array e a posicao j do array sendo j gerado aleatoriamente)
+function shuffle(array){
+    for (let i = (array.length-1); i > 0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
 let listaAmigos = [];
